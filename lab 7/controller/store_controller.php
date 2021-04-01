7655702689f21db1a31551cbda927445f0768401
Rawzerin <38815165+Rawzerin@users.noreply.github.com>
2021-04-02 00:12:21 +0600

    Delete lab 7 directory

--- lab 7/controller/store_controller.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require $_SERVER['DOCUMENT_ROOT'] . "/model/store_model.php";
-
-function getAllProducts(){
-    return retrieveAllProducts();
-}
--- lab 7/scripts/profile_view.js
+++
@@ -1,5 +0,0 @@
-let profileEditButton = document.getElementById('edit_profile').addEventListener('click',applyProfileEdits)
-
-function applyProfileEdits(){
-    window.location.href = "../view/player_edit_profile.php";
-}
